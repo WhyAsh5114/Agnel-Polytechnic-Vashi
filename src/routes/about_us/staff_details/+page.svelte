4d88de91ad77d09af429f199f0859dc0afc3e40c
Mohd. Janisar <66356535+MohdJanisar8604@users.noreply.github.com>
2025-09-10 13:40:15 +0530

    Update +page.svelte

--- src/routes/about_us/staff_details/+page.svelte
+++ src/routes/about_us/staff_details/+page.svelte
@@ -50,7 +50,7 @@
 
 	let Artificial_Intelligence_Machine_Learning_teaching_staff = [
 		['Ms. Sonali Sherigar', 'Head of Department', ['Masters in Technology'], '24/6/1995', '32 years'],
-		['Mr. Pranavkumar Bhadane', 'Lecturer', ['M.E Computer '], '17/09/2022', '0 years'],
+		['Mr. Pranavkumar Bhadane', 'Lecturer', ['M.E Computer '], '14/02/2022', '12 years'],
 		['Mrs. Ashwini Baviskar', 'Lecturer', ['B.E IT'], '01/02/2023', '3 years'], 
 		['Mrs. Samina Siddique', 'Lecturer', ['M.Tech Computer (Pursuing)'], '01/09/2023', '11 years'],
 		['Ms. Kirti Karande', 'Lecturer', ['M.E Computer (Pursuing)'], '15/7/2024', '10 years'],
