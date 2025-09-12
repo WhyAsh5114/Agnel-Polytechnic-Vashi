5ad657b5c1fe3c52de14869012456f7898f359bb
pranavkumar1991 <58133069+pranavkumar1991@users.noreply.github.com>
2025-09-12 13:08:11 +0530

    Update Events.svelte

--- src/routes/Events.svelte
+++ src/routes/Events.svelte
@@ -12,12 +12,34 @@
 		</div>
 	</div>
 <div class="card lg:card-side bg-accent text-white max-h-80 shadow-xl max-w-2xl w-full">
+		
+		<figure><img src="/events/resonance.webp" alt="Resonance 2025" /></figure>
+		<div class="card-body lg:basis-5/12 shrink-0">
+			<h2 class="card-title text-xl lg:text-3xl">Resonance 2025</h2>
+			<p class="text-md lg:text-lg lg:pt-3">20th Feb - 24th Feb</p>
+			<div class="card-actions justify-end">
+				<a class="btn btn-primary btn-sm" href="/gallery/ResoZest2025">Gallery</a>
+			</div>
+		</div>
+	</div>
+<div class="card lg:card-side bg-accent text-white max-h-80 shadow-xl max-w-2xl w-full">
 		<figure><img src="/events/zest.webp" alt="Zest/-/-2025" /></figure>
 		<div class="card-body lg:basis-5/12 shrink-0">
 			<h2 class="card-title text-xl lg:text-3xl">CodeKratz 2025</h2>
 			<p class="text-md lg:text-lg lg:pt-3">25th August</p>
 			<div class="card-actions justify-end">
 				<a class="btn btn-primary btn-sm" href="/gallery/CodeKratz 2025">Gallery</a>
+			</div>
+		</div>
+	</div>
+<div class="card lg:card-side bg-accent text-white max-h-80 shadow-xl max-w-2xl w-full">
+		
+		<figure><img src="/events/resonance.webp" alt="Resonance 2025" /></figure>
+		<div class="card-body lg:basis-5/12 shrink-0">
+			<h2 class="card-title text-xl lg:text-3xl">Resonance 2025</h2>
+			<p class="text-md lg:text-lg lg:pt-3">20th Feb - 24th Feb</p>
+			<div class="card-actions justify-end">
+				<a class="btn btn-primary btn-sm" href="/gallery/ResoZest2025">Gallery</a>
 			</div>
 		</div>
 	</div>
