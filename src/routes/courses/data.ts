1014dd627b4b79ef8c77a843537c7ae68f3b9f3d
pranavkumar1991 <58133069+pranavkumar1991@users.noreply.github.com>
2025-09-12 09:17:21 +0530

    Update data.ts

--- src/routes/courses/data.ts
+++ src/routes/courses/data.ts
@@ -178,8 +178,8 @@ export const data: Record<string, DeptData> = {
 			'M.E Mechanical',
 			'M.Tech Machine Design',
 			'M.E Manufacturing System',
-			'B.Tech Production Engineering',
-			'M.Tech Production Engineering',
+			'B.Tech Production ',
+			'M.Tech Production ',
 			'B.Tech Production',
 			'M.Sc in Chemistry'
 		],
