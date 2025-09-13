a1eb50433b4a96b143d6c90ef1832b28a6b2c8cc
pranavkumar1991 <58133069+pranavkumar1991@users.noreply.github.com>
2025-09-13 08:56:58 +0530

    Update +page.svelte

--- src/routes/about_us/prevention_of_sexual_harassment/+page.svelte
+++ src/routes/about_us/prevention_of_sexual_harassment/+page.svelte
@@ -9,7 +9,7 @@
 		['Dr. Dipti Joshi', 'Member from NGO Sreemukti Sanghatana', '8369765171'],
 		['Ms. Anoushka Bhaskarwar', 'Student (TE-3)', '8928298879'],
 		['Mr. Varun P.', '	Student (ME-3I)', '9326858981'],
-		['Ms. Kavya Shelkar', 'Student (CE-5)', '8591802121']
+		['Ms. Kavya Shelkar', 'Student (CE-3)', '8591802121']
 	];
 </script>
 
