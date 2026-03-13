edd2b2ab2ae5da4eb72e3d3cb7286d11395dc893
PRANAVKUMAR BHADANE <58133069+pranavkumar1991@users.noreply.github.com>
2026-03-13 09:57:00 +0530

    Update +page.svelte

--- src/routes/about_us/ladies_grievance/+page.svelte
+++ src/routes/about_us/ladies_grievance/+page.svelte
@@ -37,10 +37,17 @@
 		in the committee and report be prepared which should be sent to the management for appropriate action.
 	</p>
 <h2 class="lg:col-span-2 divider-heading">Grievance Redressal Committee (GRC)</h2>
+
 <p>
 In Compliance with the AICTE Notification dated 25th May 2012, published in Gazette of India, 
 we have constituted “Grievance Redressal Committee” to ensure transparency in admission, 
 with paramount objective of preventing unfair practices and to provide a mechanism to address grievances of students, parents and others.
-	</p>
-<a href="static/2025-26 grivence redressal committee_staff.pdf" class="btn btn-ghost flex-nowrap gap-2">
+</p>
+
+<a href="static/2025-26 grivence redressal committee_staff.pdf" 
+   class="btn btn-ghost flex-nowrap gap-2" 
+   target="_blank" 
+   download>
+   Download Committee PDF
+</a>
 </div>
